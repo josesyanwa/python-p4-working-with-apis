a9da87689a5bf477d5221b6e4fe5cd990c2a9a73
Joses Syanwa <josesyanwa@gmail.com>
2024-01-23 18:04:31 +0300

    Completed

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -12,3 +12,12 @@ class GetPrograms:
 
 programs = GetPrograms().get_programs()
 print(programs)
+
+def program_school(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+    programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            programs_list.append(program["agency"])
+
+        return programs_list
